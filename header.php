fb47554fe5251f53aba23caec25a0702e6a95284
dreamCode <np03a170120@heraldcollege.edu.np>
2022-01-28 14:44:36 +0545

    responsive started

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
         rel="stylesheet">
+    <link rel="stylesheet" href="./css/responsive.css">
     <title>Chitto-Paisa</title>
 </head>
 
@@ -20,16 +21,16 @@
     <header class="header_main">
         <div class="container">
             <div class="row">
-                <div class="col-md-2 flex flex_j-c-l">
+                <div class="col-md-2 col-2 flex flex_j-c-l">
                     <div class="header_left">
                         <div class="logo logo--small">
                             <img src="./images/small_logo.png" alt="">
                         </div>
                     </div>
                 </div>
-                <div class="col-md-8">
+                <div class="col-md-8 col-10">
                     <div class="header_navigation">
-                        <nav class="navbar navbar-expand-lg">
+                        <nav class="navbar navbar-expand-lg navbar-light">
                             <button class="navbar-toggler" type="button" data-toggle="collapse"
                                 data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
                                 aria-label="Toggle navigation">
@@ -69,7 +70,7 @@
                         </nav>
                     </div>
                 </div>
-                <div class="col-md-2 flex flex_j-c-r">
+                <div class="col-md-2 col-2 flex flex_j-c-r">
                     <div class="login">
                         <a href="#">login</a>
                     </div>
